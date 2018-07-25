6a3cea2b5a14823769640fbbfc13669cd19d201e
wisdomDream <1223558919@qq.com>
2018-07-25 11:08:37 +0800

    类型库定义
    类型库定义

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,15 @@
-declare module "parameter" {
-  export class Parameter {
-    validate<T>(rule: Plaint<T>, o: T): any;
-  }
-  type Plaint<T> = { [u in keyof T]: "string" | "number" | "boolean" };
+
+type typeDes="bool"|"date"|"dateTime"|"email"|"url"|"id"| "string" | "number" | "boolean"|"array"|"int"|"json"|"object";
+type childDes={
+  type:typeDes,
+  itemType:typeDes,
+  required:boolean,
+  rule:Plaint,
+  min?:number,
+  allowEmpty?:boolean,
+  format:RegExp,
+  values?:string[]
 }
+type Plaint = { //|typeof T[U]<T>|Plaint<T[U]>
+  [u :string]:any[]|childDes|typeDes |childDes//{message:string }
+ };
